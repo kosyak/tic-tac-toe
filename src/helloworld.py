4824579903dcc047ca7efec25c171e3db08fa745
Vadim Kosov <vc.kosyak@gmail.com>
2011-01-22 03:38:51 +0300

    added files for new main page; helloworld is edited to properly display it. other info and suggestions are in README

--- src/helloworld.py
+++ src/helloworld.py
@@ -10,15 +10,18 @@ import random
 class MainPage(webapp.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write("""
-      <html>
-        <body>
-          <form action="/gamestart" method="post">
-            <div><textarea name="content" rows="3" cols="60">player1 player2</textarea></div>
-            <div><input type="submit" value="play"></div>
-          </form>
-        </body>
-      </html>""")
+#        self.response.out.write("""
+#      <html>
+#        <body>
+#          <form action="/gamestart" method="post">
+#            <div><textarea name="content" rows="3" cols="60">player1 player2</textarea></div>
+#            <div><input type="submit" value="play"></div>
+#          </form>
+#        </body>
+#      </html>""")
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'html/main.html')
+        self.response.out.write(template.render(path, {}))
 
 def getUid(name):
     res = 0
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
-  $paddingX = $(document).width();
-  $paddingY = $(document).height();
+  $paddingX = $(document).width()*0.99;
+  $paddingY = $(document).height()*0.99;
   $('#login form > input').each(function() {
     $paddingX -= $(this).outerWidth();
-    $paddingY -= $(this).outerHeight();
+    $paddingY -= $(this).innerHeight();
   });
   $paddingX /= 2; 
   $paddingY /= 2;
-  $('#login').css({'padding-left': $paddingX + 'px',
-                   'padding-top':  $paddingY + 'px'});
+  $('#login').css({'padding':   $paddingY + 'px ' + 
+                   /*'padding-left':*/  $paddingX + 'px'
+                   /*'padding-bottom':($(document).height()-$paddingY) + 'px',
+                   'padding-right': ($(document).width()-$paddingX) + 'px',*/
+                   });
+  $('#throbber').css({'padding-left': ($('#login').width()*0.5-32)+'px'});
+                   
+  $('#login form > input[type="submit"]').click(function() {
+    return true;
+    $(this).parent().fadeOut('slow', function() {
+      $('#throbber').fadeIn('fast');
+      $('#login > p').fadeIn('fast');
+    });
+    return false;
+  })
 });
