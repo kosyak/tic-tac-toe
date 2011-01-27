3548ab2e696cd1803230ba8580ba9d83ef654e33
Rodion Permin <rodion.permin@gmail.com>
2011-01-27 16:11:09 +0300

    a bit refactoring: splitting project into files

---
+++ src/MainPage.py
@@ -0,0 +1,19 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        #self.response.headers['Content-Type'] = 'text/plain'
+#        self.response.out.write("""
+#      <html>
+#        <body>
+#          <form action="/gamestart" method="post">
+#            <div><textarea name="content" rows="3" cols="60">player1 player2</textarea></div>
+#            <div><input type="submit" value="play"></div>
+#          </form>
+#        </body>
+#      </html>""")
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'html/main.html')
+        self.response.out.write(template.render(path, {}))
---
+++ src/gameConstants.py
@@ -0,0 +1,8 @@
+'''
+Created on 27.01.2011
+
+@author: Rodion
+'''
+
+DIFF_TIME = 20.0
+SIZE_OF_BOARD = 5
